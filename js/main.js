633d97df50c081e3c9be1dd6750d4547017f4298
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-05-01 08:41:09 -0300

    atualiza

---
+++ js/main.js
@@ -0,0 +1,19 @@
+class Pessoa {
+    nome
+    idade
+    peso 
+} 
+
+let pessoa1  = new Pessoa()
+let pessoa2  = new Pessoa()
+
+pessoa1.nome = 'Luan'
+pessoa1.idade = 26
+pessoa1.peso = 84.5
+
+pessoa2.nome = 'Vitorio'
+pessoa2.idade = 20
+pessoa2.peso = 80.5
+
+console.log(pessoa1)
+console.log(pessoa2)
