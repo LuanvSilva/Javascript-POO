32fc164b003a9546143652944a16df8a6271dea9
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-05-01 09:18:46 -0300

    atualiza

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 class Pessoa {
-    nome
-    idade
-    peso 
+    _nome
+    _idade
+    _peso 
+    _altura
+    _imc
+    constructor (nome, idade,peso,altura){
+        this._nome = nome
+        this._idade = idade
+        this._peso = peso
+        this._altura = altura
+        this._imc = this._peso / (this._altura * this._altura)
+    }
+    calculaImc(){
+        return (this._peso / (this._altura * this._altura)).toFixed(2)
+    }
 } 
 
-let pessoa1  = new Pessoa()
-let pessoa2  = new Pessoa()
+let pessoa1  = new Pessoa('Lunar',25,80.5,1.90)
+let pessoa2  = new Pessoa('Marte',26,50.5,1.50)
+let pessoa3  = new Pessoa('Satu',27,82.2,1.90)
+let pessoa4  = new Pessoa('Barata',90.2,50,1.50)
 
-pessoa1.nome = 'Luan'
-pessoa1.idade = 26
-pessoa1.peso = 84.5
 
-pessoa2.nome = 'Vitorio'
-pessoa2.idade = 20
-pessoa2.peso = 80.5
 
-console.log(pessoa1)
+
+
+console.log(pessoa1.calculaImc())
 console.log(pessoa2)
+console.log(pessoa3)
+console.log(pessoa4)
